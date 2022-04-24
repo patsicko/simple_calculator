96846c7b521c662b4af1f5a30a7569f51868e524
patsicko <patsicko@g>
2022-04-24 08:36:33 +0200

    Feat:Calculator -Change js [Finishes]

--- index.js
+++ index.js
@@ -1,25 +1,54 @@
+// let display=document.getElementById("display");
+// let allButtons=Array.from(document.getElementsByClassName("button"));
+// allButtons.map( function (pressed)  {
+//     pressed.addEventListener('click', (event) => {
+//         switch(event.target.innerText){
+//             case 'C':
+//                 display.innerText = '';
+//                 break;
+//             case '=':
+//                 try{
+//                     display.innerText = eval(display.innerText);
+//                 } catch {
+//                     display.innerText = "Error"
+//                 }
+//                 break;
+//             case '←':
+//                 if (display.innerText){
+//                    display.innerText = display.innerText.slice(0, -1);
+//                 }
+//                 break;
+//             default:
+//                 display.innerText += event.target.innerText;
+//         }
+//     });
+// });
+
 let display=document.getElementById("display");
-let buttons=Array.from(document.getElementsByClassName("button"));
-buttons.map( button => {
-    button.addEventListener('click', (e) => {
-        switch(e.target.innerText){
-            case 'C':
-                display.innerText = '';
-                break;
-            case '=':
-                try{
-                    display.innerText = eval(display.innerText);
-                } catch {
-                    display.innerText = "Error"
-                }
-                break;
-            case '←':
-                if (display.innerText){
-                   display.innerText = display.innerText.slice(0, -1);
-                }
-                break;
-            default:
-                display.innerText += e.target.innerText;
-        }
-    });
+let allButtons=Array.from(document.getElementsByClassName("button"));
+ let action=function(event){
+    switch(event.target.innerText){
+        case 'C':
+            display.innerText = '';
+            break;
+        case '=':
+            try{
+                display.innerText = eval(display.innerText);
+            } catch {
+                display.innerText = "Error"
+            }
+            break;
+        case '←':
+            if (display.innerText){
+               display.innerText = display.innerText.slice(0, -1);
+            }
+            break;
+        default:
+            display.innerText += event.target.innerText;
+    }
+}
+allButtons.map( function (pressed)  {
+    pressed.addEventListener('click', action); 
 });
+
+
