71d4116c11f71517c0858181a983af26ee099470
patsicko <patsicko@g>
2022-04-26 04:32:05 +0200

    Feat:Calculator -Write new codes bu using arrow function [Finishes#]

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // let display=document.getElementById("display");
 // let allButtons=Array.from(document.getElementsByClassName("button"));
-// allButtons.map( function (pressed)  {
-//     pressed.addEventListener('click', (event) => {
+// allButtons.map( function (button)  {
+//     button.addEventListener('click', (event) => {
 //         switch(event.target.innerText){
 //             case 'C':
 //                 display.innerText = '';
@@ -9,13 +9,14 @@
 //             case '=':
 //                 try{
 //                     display.innerText = eval(display.innerText);
-//                 } catch {
+//                 } catch(err) {
 //                     display.innerText = "Error"
+//                     console.log(err);
 //                 }
 //                 break;
 //             case '←':
 //                 if (display.innerText){
-//                    display.innerText = display.innerText.slice(0, -1);
+//                    display.innerText = display.innerText.slice(0, -1);//array=[1,2,3,4,5,6,7,8,9,10];
 //                 }
 //                 break;
 //             default:
@@ -24,31 +25,55 @@
 //     });
 // });
 
+// let display=document.getElementById("display");
+// let allButtons=Array.from(document.getElementsByClassName("button"));
+//  let action=function(event){
+//     switch(event.target.innerText){
+//         case 'C':
+//             display.innerText = '';
+//             break;
+//         case '=':
+//             try{
+//                 display.innerText = eval(display.innerText);
+//             } catch {
+//                 display.innerText = "Error"
+//             }
+//             break;
+//         case '←':
+//             if (display.innerText){
+//                display.innerText = display.innerText.slice(0, -1);
+//             }
+//             break;
+//         default:
+//             display.innerText += event.target.innerText;
+//     }
+// }
+// allButtons.map( function (pressed)  {
+//     pressed.addEventListener('click', action); 
+// });
+
+
 let display=document.getElementById("display");
 let allButtons=Array.from(document.getElementsByClassName("button"));
- let action=function(event){
-    switch(event.target.innerText){
-        case 'C':
-            display.innerText = '';
-            break;
-        case '=':
-            try{
-                display.innerText = eval(display.innerText);
-            } catch {
-                display.innerText = "Error"
-            }
-            break;
-        case '←':
-            if (display.innerText){
-               display.innerText = display.innerText.slice(0, -1);
-            }
-            break;
-        default:
-            display.innerText += event.target.innerText;
-    }
-}
-allButtons.map( function (pressed)  {
-    pressed.addEventListener('click', action); 
-});
-
-
+allButtons.map((button)=>{
+    button.addEventListener("click", (e)=>{
+        switch (e.target.innerText) {
+            case "C":
+                display.innerText="";
+                break;
+            case "=":
+                try{
+                    display.innerText=eval(display.innerText);
+                }
+            catch(err){
+                display.innerText="Error!";
+            }   
+            break;     
+            case "←":
+                display.innerText=display.innerText.slice(0,-1);
+                break;
+            default:
+                display.innerText+=e.target.innerText;
+        }
+    })
+})
